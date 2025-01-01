4c12b04d75f25ee66d8e2c86b7e790c9ad3e7488
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-01 20:48:30 +0200

    add custom scroll view

--- lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -17,60 +17,56 @@ class BookDetailsViewBody extends StatelessWidget
 
     var width = MediaQuery.of(context).size.width;
 
-    return Column(
-    
-    children:
-    [
-      
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: const CustomBookDetialsAppBar()
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Column(
+            children: [
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: const CustomBookDetialsAppBar()),
 
-      //const SizedBox(height: 3,),
+              //const SizedBox(height: 3,),
 
-      //BY Meeeeeeeeeeeeeeeee
-        Padding(
-          padding: EdgeInsetsDirectional.only(start: 15, end: 15),
-          child: Container(width: double.infinity, height: 1, color: AssetColors().kSecondrayColor,)
-        ),
-        //BY Meeeeeeeeeeeeeeeee
-      const SizedBox(height: 10,),
+              //BY Meeeeeeeeeeeeeeeee
+              Padding(padding: EdgeInsetsDirectional.only(start: 15, end: 15),
+                  child: Container(width: double.infinity, height: 1, color: AssetColors().kSecondrayColor,)),
+              //BY Meeeeeeeeeeeeeeeee
+              const SizedBox(height: 10,),
 
+              //63
+              SizedBox(height: MediaQuery.of(context).size.height * 0.35,
+                  child: CustomBookImage()),
 
-      //63
-      SizedBox(
-        height: MediaQuery.of(context).size.height * 0.35,
-        child: CustomBookImage()
-      ),
+              const SizedBox(height: 35,),
+              Text("The Jungle Book", style: Styles.titleStyle30,),
+              const SizedBox(height: 3,),
+              Text("Rudyard Kipling", style: Styles.titleStyle18),
+              const SizedBox(height: 16,),
+              BookRating(mainAxisAlignment: MainAxisAlignment.center,),
 
-      const SizedBox(height: 35,),
-      Text("The Jungle Book", style: Styles.titleStyle30,),
-      const SizedBox(height: 3,),
-      Text("Rudyard Kipling", style: Styles.titleStyle18),
-      const SizedBox(height: 16,),
-      BookRating(mainAxisAlignment: MainAxisAlignment.center,),
+              const SizedBox(height: 35,),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: BooksAction()
+                ),
 
-      const SizedBox(height: 35,),
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
-        child: BooksAction()
-      ),
+              const Expanded(child: SizedBox(height: 50)),
+              Padding(padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Align(alignment: Alignment.centerLeft,
+                    child: Text("You can Also Like:", style: Styles.titleStyle14.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )),
+              ),
 
-      const SizedBox(height: 50),
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Text("You can Also Like:", style: Styles.titleStyle14.copyWith(
-            fontWeight: FontWeight.w600,),)
+              const SimilarBooksListView(),
+              //const SizedBox(height: 40,),
+            ],
           ),
-        ),
-
-      const SimilarBooksListView(),
-      //const SizedBox(height: 40,),
-    ],
+        )
+      ],
     );
   }
 }
-
