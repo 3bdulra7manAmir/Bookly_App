0ba259bea30f99296d64393d756b00f684180ec6
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-02 22:18:00 +0200

    implement fetch newest books

--- lib/Features/Home/presentation/views/data/repos/home_repo_impl.dart
+++ lib/Features/Home/presentation/views/data/repos/home_repo_impl.dart
@@ -1,17 +1,41 @@
 import 'package:bookly/Features/home/presentation/views/data/models/book_model/book_model.dart';
 import 'package:bookly/Features/home/presentation/views/data/repos/home_repo.dart';
 import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo
+{
+  HomeRepoImpl(this.apiService);
+
+  final ApiService apiService;
+
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async
+  {
+    try
+    {
+      var data = await apiService.get(endPoints: 'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming');
+
+      List<BookModel> books = [];
+      for (var item in data['items'])
+      {
+        books.add(BookModel.fromJson(item));
+      }
+      
+      return right(books);
+    }
+    on Exception catch (e)
+    {
+      print(e);
+      return left(ServerFailure());
+    }
+    
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() {
     // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -2,14 +2,14 @@ import 'package:dio/dio.dart';
 
 class ApiService
 {
-  final Dio dio;
-  ApiService(this.dio);
+  ApiService(this._dio);
 
+  final Dio _dio;
   final String _baseUrl = 'https://www.googleapis.com/books/v1/';
 
   Future<Map<String,dynamic>> get({required String endPoints}) async
   {
-    var response = await dio.get('$_baseUrl$endPoints');
+    var response = await _dio.get('$_baseUrl$endPoints');
     return response.data;
   }
   
