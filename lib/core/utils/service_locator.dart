ed7f90912dbe9d1fbdb750de2fb1f37161eb334b
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-04 14:15:30 +0200

    add setup service locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/Features/home/presentation/views/data/repos/home_repo_impl.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt getIt = GetIt.instance;
+
+void setupServiceLocator()
+{
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(getIt.get<ApiService>()));
+}
+
+class AuthRepo
+{
+  final ApiService apiService;
+  AuthRepo(this.apiService);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,40 @@
+import 'package:bookly/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/manager/newest_books_cubit/cubit/newest_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/views/data/repos/home_repo_impl.dart';
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
-// import 'package:get/get_navigation/src/root/get_material_app.dart';
-//import 'package:bookly/Features/Splash/presentation/views/splash_view.dart';
-//import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main()
+{
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
-class BooklyApp extends StatelessWidget {
+class BooklyApp extends StatelessWidget
+{
   const BooklyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: AssetColors().kPrimaryColor,
-        textTheme: GoogleFonts.montserratAlternatesTextTheme(
-            ThemeData.dark().textTheme),
+  Widget build(BuildContext context)
+  {
+    return MultiBlocProvider(
+      providers:
+      [
+        BlocProvider(create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>())),
+        BlocProvider(create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>())),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: AssetColors().kPrimaryColor,
+          textTheme: GoogleFonts.montserratAlternatesTextTheme(
+              ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
